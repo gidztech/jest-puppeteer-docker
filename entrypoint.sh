8194ebee90ce027161e67991ad4ef4bd764113ea
gidztech <gjpyzer+github@gmail.com>
2018-11-22 02:36:51 +0000

    Revert network mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# Make sure there's a host entry
 HOST_DOMAIN="host.docker.internal"
+DOCKER_IP="$(getent hosts host.docker.internal | awk '{ print $1 }')"
+echo $DOCKER_IP " " $HOST_DOMAIN >> /etc/hosts
+
 ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   # Try using default interface
@@ -8,11 +12,11 @@ if [ $? -ne 0 ]; then
   ping -q -c1 $DOCKER_IP > /dev/null 2>&1
   if [ $? -eq 0 ]; then
       # Default interface was good so patch hosts
-      echo -e $DOCKER_IP "\t$HOST_DOMAIN" >> /etc/hosts
+      echo $DOCKER_IP " " $HOST_DOMAIN >> /etc/hosts
   else 
       # Try eth0 instead and then patch hosts
       DOCKER_IP="$(ip addr show eth0 | grep 'inet ' | awk '{ print $2}' | cut -d'/' -f1)"
-      echo -e $DOCKER_IP "\t$HOST_DOMAIN" >> /etc/hosts
+      echo $DOCKER_IP " " $HOST_DOMAIN >> /etc/hosts
   fi  
 fi
 
